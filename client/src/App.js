23270ca2754a773fd44043e9995fbf4ecbe106f7
Krist Veruari <kveruari@gmail.com>
2021-09-07 23:21:50 -0600

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import Login from "./Login"
 import Dashboard from "./Dashboard"
+import "./App.css"
 
 const code = new URLSearchParams(window.location.search).get('code')
 
 
 function App() {
-  return code ? <Dashboard code={code} /> : <Login />
-    
+  return (
+    code ? <Dashboard code={code} /> : <Login />
+  )
 }
 
 export default App;
